0336bb2f3fb19e36c8207d65b94a70b787282b81
Hudya <mhudyaramadhana@gmail.com>
2021-08-08 20:19:16 +0700

    Add singleton to Response file

--- part_3-rest_crud/app/response.py
+++ part_3-rest_crud/app/response.py
@@ -1,6 +1,13 @@
 from flask import make_response, jsonify
 
 class Response(object):
+    
+    __instance = None
+
+    def __new__(cls):
+        if Response.__instance is None:
+            Response.__instance = object.__new__(cls)
+        return Response.__instance
 
     def __init__(self):
         self.code_status = 200
@@ -28,3 +35,5 @@ class Response(object):
         self.code_status = 404
 
         return self.create_payload_response(message, values)
+
+response = Response()
