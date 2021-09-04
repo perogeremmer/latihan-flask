37c7990209b2778cc1cfd1f1e0784409a0c01e37
Hudya <mhudyaramadhana@gmail.com>
2021-09-04 10:46:18 +0700

    Add delete test

--- part_7-functional-and-unit-test/tests/functional/test_functional_todo copy.py
+++ part_7-functional-and-unit-test/tests/functional/test_functional_todo copy.py
@@ -390,4 +390,71 @@ def test_update_todo_with_different_user(client, database):
     response = client.put(f'/api/todo/{id}', json=payload, headers={'Authorization': token_user})
     assert response.status_code == 400
 
+def test_delete_todo(client, database):
+    payload = {
+        "name": "John",
+        "email": "john@example.com",
+        "password": "12345678",
+        "confirmation_password": "12345678",
+    }
+    response = client.post('/api/register', json=payload)
 
+    data = json.loads(response.get_data(as_text=True))
+    token = "Bearer {}".format(data['values']['token']['access_token'])
+
+    payload = {
+        "title": "Mencuci motor",
+        "description": "Mencuci Motor supra",
+    }
+    response = client.post('/api/todo', json=payload, headers={'Authorization': token})
+    assert response.status_code == 200
+
+    data = json.loads(response.get_data(as_text=True))
+    assert data['values']['title'] == "Mencuci motor"
+    assert data['values']['done'] == False
+
+    id = data['values']['id']
+
+    payload = {}
+    response = client.delete(f'/api/todo/{id}', json=payload, headers={'Authorization': token})
+    assert response.status_code == 200
+
+
+def test_delete_todo_with_different_user(client, database):
+    payload = {
+        "name": "John",
+        "email": "john@example.com",
+        "password": "12345678",
+        "confirmation_password": "12345678",
+    }
+    response = client.post('/api/register', json=payload)
+
+    data = json.loads(response.get_data(as_text=True))
+    token = "Bearer {}".format(data['values']['token']['access_token'])
+
+    payload_user = {
+        "name": "Hudya",
+        "email": "perogeremmer@mail.com",
+        "password": "12345678",
+        "confirmation_password": "12345678",
+    }
+    response_user = client.post('/api/register', json=payload_user)
+    data = json.loads(response_user.get_data(as_text=True))
+    token_user = "Bearer {}".format(data['values']['token']['access_token'])
+
+    payload = {
+        "title": "Mencuci motor",
+        "description": "Mencuci Motor supra",
+    }
+    response = client.post('/api/todo', json=payload, headers={'Authorization': token})
+    assert response.status_code == 200
+
+    data = json.loads(response.get_data(as_text=True))
+    assert data['values']['title'] == "Mencuci motor"
+    assert data['values']['done'] == False
+
+    id = data['values']['id']
+
+    payload = {}
+    response = client.delete(f'/api/todo/{id}', json=payload, headers={'Authorization': token_user})
+    assert response.status_code == 400
